e5bc2bde450e26243ca7937bbaa841f1e6d38a9e
SatY213 <abdelhakim_merini@outlook.fr>
2024-02-12 10:27:09 +0100

    push

--- models/User.js
+++ models/User.js
@@ -15,11 +15,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  role: {
-    type: String,
-    required: true,
-    enum: ["admin", "subscriber"],
-  },
 });
 
 const User = mongoose.model("User", userSchema);
